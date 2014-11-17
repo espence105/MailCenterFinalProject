011c2821bc3ffa489799cc43c4017af4162db690
Rusty Shackleford <rushackleford182@gmail.com>
2014-11-16 22:07:56 -0500

    changed insert slightly, started work on update and delete

--- dataBase.py
+++ dataBase.py
@@ -9,34 +9,89 @@ class DataBase(object):
 
     
     def __init__(self):
-        self.con = lite.connect('clientDB2.db')
+        pass
 
     def insert_client(self, client_info):
-
+        
+        con = lite.connect('clientDB2.db')
         try:
-            with self.con:
-                cur = self.con.cursor()
+            with con:
+                cur = con.cursor()
                 cur.executemany("INSERT INTO client VALUES(?,?,?,?,?,?,?,?,?)", (client_info,))
-                self.con.commit
+                con.commit
                 
         except lite.Error, e:
-            if self.con:
-                self.con.rollback()
+            if con:
+                con.rollback()
 
             print "Error %s:" % e.args[0]
             sys.exit(1)
 
         finally:
-            if self.con:
-                self.con.close()
+            if con:
+                con.close()
+
+        ## IN PROGRESS ##
+
+    """def update_client(self, client_info):
+
+        con = lite.connect('clientDB2.db')
+        try:
+            with con:
+                cur = self.con.cursor()
+                cur.execute("SELECT * FROM client")
+
+                rows = cur.fetchall()
+
+                for row in rows:
+                    print row
+                    
+        except lite.Error, e:
+            if con:
+                con.rollback()
+
+            print "Error %s:"  % e.args[0]
+            sys.exit()
+
+        finally:
+            if con:
+                con.close()"""
+
+    """ def select_client(self, client_info)
+        con = lite.connect('clientDB2.db')"""
+        
+    """def delete_client(self, client_info):
+        con = lite.connect('clientDB2.db')
+        try:
+            with con:
+                cur = self.con.cursor()
+                cur.execute("DELETE FROM client WHERE fName == 'bob'")
+
+                rows = cur.fetchall()
+
+                for row in rows:
+                    print row
+                    
+        except lite.Error, e:
+            if con:
+                con.rollback()
+
+            print "Error %s:"  % e.args[0]
+            sys.exit()
+
+        finally:
+            if con:
+                con.close()"""
 
 
-#def main():
-    #DB = DataBase()
-    
-    #client_info = (None, 'bob', 'bob', '1111111', 'bob@bob.bob', 'bob street', 'bobtopia', 'bobland', '00002')
-    #DB.insert_client(client_info)
 
+## FOR TESTING ##
+"""def main():
+    DB = DataBase()
 
-#if __name__ == '__main__':
-    #main()
+    client_info = (None, 'bob', 'bob', '1111111', 'bob@bob.bob', 'bob street', 'bobtopia', 'bobland', '00002')
+
+    DB.update_client(client_info)
+
+if __name__ == '__main__':
+    main()"""
