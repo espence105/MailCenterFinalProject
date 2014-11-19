65207ed4946d12f16f7d948bc5d11045e9ea0077
Rusty Shackleford <rushackleford182@gmail.com>
2014-11-18 19:33:02 -0500

    Wrote select and delete statements, fixed update

--- dataBase.py
+++ dataBase.py
@@ -37,10 +37,10 @@ class DataBase(object):
             if con:
                 con.close()
 
-        ## IN PROGRESS ##
+    
 
     #Update info in the client table
-    """def update_client(self, update_info):
+    def update_client(self, update_info):
         #Connect to database.  Will also create new database if it does not exist.
         con = lite.connect('clientDB2.db')
         try:
@@ -51,9 +51,9 @@ class DataBase(object):
                 
                 cur.executemany('UPDATE client SET email=?, forwardingAddress=?, city=?, state=?, zipCode=? WHERE studentID = ?', (update_info,))
 
-                #cur.execute('SELECT * FROM client WHERE studentID = ?', (update_info[5]))
-                #row = cur.fetchall()
-                #return row
+                cur.execute('SELECT * FROM client WHERE studentID = ?', (update_info[5],))
+                row = cur.fetchall()
+                return row
                     
         except lite.Error, e:
             if con:
@@ -64,22 +64,41 @@ class DataBase(object):
 
         finally:
             if con:
-                con.close()"""
+                con.close()
 
-    """ def select_client(self, client_info)
-        con = lite.connect('clientDB2.db')"""
+    def select_client(self, client_id):
+        con = lite.connect('clientDB2.db')
+
+        try:
+            with con:
+                cur = con.cursor()
+                #Create the client table if it does not already exist
+                cur.execute('CREATE TABLE IF NOT EXISTS client(fName TEXT NOT NULL, lName TEXT NOT NULL, studentID TEXT NOT NULL, email TEXT NOT NULL, forwardingAddress TEXT NOT NULL, city TEXT NOT NULL, state TEXT NOT NULL, zipCode TEXT NOT NULL, gradDate TEXT NOT NULL)')
+
+                cur.execute('SELECT * FROM client WHERE studentID = ?', (client_id,))
+
+                row = cur.fetchall()
+                return row
+        except lite.Error, e:
+            print "Error %s:" % e.args[0]
+            sys.exit()
+
+        finally:
+            if con:
+                con.close()
         
-    """def delete_client(self, client_info):
+    def delete_client(self, delete_info):
         con = lite.connect('clientDB2.db')
         try:
             with con:
+                
+                #Create the client table if it does not already exist
+                cur.execute('CREATE TABLE IF NOT EXISTS client(fName TEXT NOT NULL, lName TEXT NOT NULL, studentID TEXT NOT NULL, email TEXT NOT NULL, forwardingAddress TEXT NOT NULL, city TEXT NOT NULL, state TEXT NOT NULL, zipCode TEXT NOT NULL, gradDate TEXT NOT NULL)')
+
+                
                 cur = self.con.cursor()
-                cur.execute("DELETE FROM client WHERE fName == 'bob'")
-
-                rows = cur.fetchall()
-
-                for row in rows:
-                    print row
+                cur.execute('DELETE FROM client WHERE studentID == ?', (delete_info,))
+                
                     
         except lite.Error, e:
             if con:
@@ -90,7 +109,7 @@ class DataBase(object):
 
         finally:
             if con:
-                con.close()"""
+                con.close()
 
 
 
@@ -99,11 +118,12 @@ class DataBase(object):
 """def main():
     DB = DataBase()
     
-    client_info = ('bob', 'bob', '1111115', 'bob@bob.bob', 'bob street', 'bobtopia', 'bobland', '00002', '1-1-01')
-    id_key = '1111115'
+    client_info = ('bob', 'bob', '1111119', 'bob@bob.bob', 'bob street', 'bobtopia', 'bobland', '00002', '1-1-01')
+    id_key = '1111119'
     #DB.insert_client(client_info)
-    update_info = ('hank@bill.boomhauer', 'rainey street', 'arlen', 'texas', '33333', '1111115')
-    DB.update_client(update_info)"""
+    update_info = ('hank@bill.boomhauer', 'rainey street', 'arlen', 'texas', '33333', '1111116')
+    print DB.update_client(update_info)
+    print DB.select_client(id_key)
 
 if __name__ == '__main__':
-    main()
+    main()"""
